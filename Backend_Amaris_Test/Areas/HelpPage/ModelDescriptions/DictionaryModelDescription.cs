c8c6536f7ac63229ca75e2216059072805e79422
Federico Creti <federico.creti87@outlook.it>
2017-10-21 14:38:22 +0200

    Add project files.

---
+++ Backend_Amaris_Test/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Backend_Amaris_Test.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Backend_Amaris_Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Backend_Amaris_Test.WebApiApplication" Language="C#" %>
